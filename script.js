2dccbc7df727183b3dba43d0e6cd04a678ebb18e
Mel Incognito <melxincognito@gmail.com>
2021-08-12 15:41:10 -0700

    single round functions

--- script.js
+++ script.js
@@ -11,3 +11,34 @@ function computerPlay() {
     }
     return computerPlay; 
 }
+
+function playerSelection() {
+    const playerSelection = prompt('Rock, paper or scissors?').toLowerCase();
+    return playerSelection; 
+}
+
+
+function game(playerSelection, computerPlay) {
+    if(playerSelection == 'rock' && computerPlay == 'scissors'){
+        return 'You Won! Rock beats scissors!';
+    } else if (playerSelection == 'rock' && computerPlay == 'paper') {
+        return 'You lost! Paper beats rock!';
+    } else if (playerSelection == 'rock' && computerPlay == 'rock') {
+        return "It's a draw!";
+    } else if (playerSelection == 'paper' && computerPlay == 'rock') {
+        return 'You win! Paper beats rock!';
+    } else if (playerSelection == 'paper' && computerPlay == 'scissors') {
+        return 'You lost! Scissors beats paper!';
+    } else if (playerSelection == 'paper' && computerPlay == 'paper') {
+        return "It's a tie!";
+    } else if (playerSelection == 'scissors' && computerPlay == 'paper') {
+        return 'You Win! Scissors beats paper!'
+    } else if (playerSelection == 'scissors' && computerPlay == 'rock') {
+        return 'You lose! Rock beats scissors!';
+    } else if (playerSelection == 'scissors' && computerPlay == 'scissors') {
+        return "It's a tie!";
+    } else {
+        return 'Something went wrong';
+    }
+}
+ console.log(game(playerSelection(),computerPlay()));
