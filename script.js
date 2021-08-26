e8b7db86c0a0088e724d5ca85d3f78914a9d6840
Mel Incognito <melxincognito@gmail.com>
2021-08-26 14:12:16 -0700

    finished functional ui

--- script.js
+++ script.js
@@ -12,64 +12,7 @@ function computerPlay() {
     }
     return computerPlay; 
 }
-/* function for a player to be prompted for their selection*/ 
-//function playerSelection() {
-//    const playerSelection = prompt('Rock, paper or scissors?').toLowerCase();
-//    return playerSelection; 
-//}
 
-/*initialize userScore and computerScore before the function*/
-
-
-/* one single round playing in the terminal */
-function playRound(playerSelection, computerPlay) {
-    if(playerSelection == 'rock' && computerPlay == 'scissors'){
-        console.log('You Won! Rock beats scissors, you get a point');
-        userScore++;
-    } else if (playerSelection == 'rock' && computerPlay == 'paper') {
-        console.log('You lost! Paper beats rock, computer gets a point!');
-        computerScore++; 
-    } else if (playerSelection == 'rock' && computerPlay == 'rock') {
-        console.log("Rock on Rock! Nobody gets a point!"); 
-    } else if (playerSelection == 'paper' && computerPlay == 'rock') {
-        console.log('You Won! Paper beats rock, you get a point!');
-        userScore++;
-    } else if (playerSelection == 'paper' && computerPlay == 'scissors') {
-        console.log('You lost! Scissors beats paper, computer gets a point!');
-        computerScore++; 
-    } else if (playerSelection == 'paper' && computerPlay == 'paper') {
-        console.log("Paper on paper! Nobody gets a point!");
-    } else if (playerSelection == 'scissors' && computerPlay == 'paper') {
-        console.log('You Won! Scissors beats paper, you get a point!');
-        userScore++;
-    } else if (playerSelection == 'scissors' && computerPlay == 'rock') {
-        console.log('You lost! Rock beats scissors, computer gets a point!');
-        computerScore++;
-    } else if (playerSelection == 'scissors' && computerPlay == 'scissors') {
-        console.log("Scissoring?? Nobody gets a point!");
-    } else {
-        console.log('Something went wrong');
-    }
-    
-    return 'Your points: ' + userScore + ' Computers Points: ' + computerScore;
-    
-}
-
-/* trying to make one that'll keep score and go up to 5 games.*/
-
-
-function game() { 
-    console.log(playRound(playerSelection(),computerPlay()));
-    /*console.log above and keep score tallied with each runthrough of playRound*/
-    /*code below should compute who the winner or loser is and how many points they won by*/
-    if (userScore > computerScore) {
-        console.log('You won by ' + (userScore - computerScore) + ' points!');
-    } else if (userScore < computerScore) {
-        console.log('You lost! Computer won by ' + (computerScore - userScore) + ' points!');
-    } else {
-        console.log('It was a tie!');
-    }
-    } 
 
     
 function vimel() {
@@ -87,7 +30,9 @@ function vimel() {
 
 let userScore = 0;
 let computerScore = 0;
+let roundWinner = prompt('What is your name?');
 
+// ui 
 
 const scissors = document.getElementById('scissors');
 const rock = document.getElementById('rock');
@@ -96,6 +41,7 @@ const playerScore = document.getElementById('playerScore');
 const compScore = document.getElementById('compScore');
 const moves = document.getElementById('moves');
 const end = document.getElementById('ending');
+const winner = document.getElementById('winner');
 
 
 rock.addEventListener('click', function() {
@@ -114,17 +60,18 @@ scissors.addEventListener('click', function() {
 });
 
 
+// game 
 
-function weaponChoice(playerSelection) {
-    game2(playerSelection, computerPlay());
+function rpsGame(playerSelection, computerPlay) {
 
+    if(gameOver()){
+        points(userScore, computerScore)
+        return
+    }
+
+    playRound2(playerSelection, computerPlay);
+   
 }
-
-function displayResults(userScore, computerScore) {
-    playerScore.textContent = `You: ${userScore}`;
-    compScore.textContent = `Computer: ${computerScore}`;
-};
-
 
 function playRound2(playerSelection, computerPlay) {
     if(playerSelection == 'rock' && computerPlay == 'scissors'){
@@ -158,22 +105,38 @@ function playRound2(playerSelection, computerPlay) {
 }
 
 
-function game2(playerSelection, computerPlay) { 
-    playRound2(playerSelection, computerPlay);
-    
-
-    /*console.log above and keep score tallied with each runthrough of playRound*/
-    /*code below should compute who the winner or loser is and how many points they won by*/
+function points(userScore, computerScore) {
     if (userScore > computerScore) {
-        end.textContent= `You won by ${userScore - computerScore} points!`;
+    end.textContent= `You won by ${userScore - computerScore} points!`;
     } else if (userScore < computerScore) {
-        end.textContent = `You lost! Computer won by ${computerScore - userScore} points!`;
+    end.textContent = `You lost! Computer won by ${computerScore - userScore} points!`;
     } else {
-        end.textContent = 'It was a tie!';
-    }
-} 
+    end.textContent = 'It was a tie!';
+    };
+}
+
+function weaponChoice(playerSelection) {
+    rpsGame(playerSelection, computerPlay());
+
+}
+
+function displayResults(userScore, computerScore) {
+    playerScore.textContent = `You: ${userScore}`;
+    compScore.textContent = `Computer: ${computerScore}`;
+};
+
+
+function gameOver() {
+    return userScore === 5 || computerScore === 5
+}
 
 
 
-/* trying to make one that'll keep score and go up to 5 games.*/
+
+
+
+
+
+
+
 
